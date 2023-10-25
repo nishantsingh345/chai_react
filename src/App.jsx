d9ebe3ba7057d4910347459d06822597278debe1
Nishant <nishantsingh95340@gmail.com>
2023-10-25 23:05:14 +0530

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Chai from "./Chai";
+import ColorChnage from "./components/ColorChnage";
 import ReduxToolkit from "./components/ReduxToolkit";
+
 
 function App() {
   return (
     <>
       <Chai />
       <ReduxToolkit />
+      <ColorChnage />
     </>
   );
 }
---
+++ src/components/ColorChnage.jsx
@@ -0,0 +1,14 @@
+const ColorChnage = () => {
+  <link rel="stylesheet" href="ColorChnage.css" />;
+  return (
+    <div className="btn_grp">
+      <p className="btn">yellow</p>
+      <p className="btn">red</p>
+      <p className="btn">blue</p>
+      <p className="btn">green</p>
+      <p className="btn">gray</p>
+    </div>
+  );
+};
+
+export default ColorChnage;
